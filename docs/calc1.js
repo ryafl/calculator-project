8852a5a4b6f3d62f338e466d29c7d50ba39da972
Ryan Flack <ryaflack@gmail.com>
2018-02-20 17:42:37 -0600

    Working Calculator with no identifiablebugs and catches all error

--- docs/calc1.js
+++ docs/calc1.js
@@ -2,6 +2,7 @@
 
 let equationArray = [];
 let currentDisplay = "";
+let decCount = 0;
 let buttons = document.querySelectorAll('.button');
 for (let x = 0; x<buttons.length; x++) {
 	buttons[x].addEventListener('click', function(e) {
@@ -14,19 +15,32 @@ for (let x = 0; x<buttons.length; x++) {
 
 function addToArray(butVal) {
 	let buttonValue = butVal.toString();
+	let decContinue = 'Yes';
+	if (buttonValue == '.') {
+		console.log(buttonValue);
+		decContinue = decimalCheck(buttonValue);
+	}
+	if (decContinue == 'no') {
+		let pleaseWork = 0;
+	}
+	else {
 	equationArray.push(buttonValue);
+	}
 	let equalsButton = false;
-	if (currentDisplay !== "") {
+	
+	if (currentDisplay !== "") { //Lets the user continue operating on the answer if theres an operator, if not it resets the equation to just the new number entered
 		if (buttonValue == '+' || buttonValue == '-' || buttonValue == '/' || buttonValue == '*') {
 			let nothing = buttonValue;
+			currentDisplay = '';
+
 
 		}
 		else {
 			equationArray = [];
 			equationArray.push(buttonValue);
-			currentDisplay == "";
+			currentDisplay = "";
 		}
-	}
+	} 
 	for (let i = 0; i<equationArray.length; i++) {
 		
 		if (equationArray[i] == 'DEL') {
@@ -36,6 +50,7 @@ function addToArray(butVal) {
 		if (equationArray[i] == '+' || equationArray[i] == '-' || equationArray[i] == '*' || equationArray[i] == '/') {
 			let placeholder =  ' ' + equationArray[i] + ' ';
 			equationArray.splice(i, 1, placeholder);
+			
 
 		}
 
@@ -43,6 +58,8 @@ function addToArray(butVal) {
 			equationArray[i] = ' =';
 			equalsButton = true;
 		}
+
+		
 	
 	}
 
@@ -86,8 +103,10 @@ function operate(stringVal) {
 		let z = k % 2;
 		if (z == 0) {
 			let numbaCheck = parseFloat(checkArray[k]);
-			if (numbaCheck == NaN) {
+			//if (numbaCheck == NaN) {
+			if (isNaN(numbaCheck) == true) {
 				returnDisplay = "Error";
+				
 				break;
 			}
 			else {
@@ -117,13 +136,20 @@ function operate(stringVal) {
 			}
 			if (operateArray[a] == '/') {
 				let divideVal = divide(operateArray[a-1], operateArray[a+1]);
+				if (divideVal == 'Error') {          //catching divide by zero
+					returnDisplay = 'Error';
+					break
+				}
+				else {
 				operateArray.splice(a-1, 3, divideVal);
 				a = 0;
 				lengthCounter -=2;
+				}
 			}
 			
 		}
 
+	if (returnDisplay !== 'Error') {
 		for (let b = 0; b<lengthCounter; b++) {
 			if (operateArray[b] == '+') {
 				let addVal = add(operateArray[b-1], operateArray[b+1]);
@@ -141,6 +167,7 @@ function operate(stringVal) {
 
 		
 		returnDisplay = operateArray[0].toString();
+	}
 
 
 	}
@@ -149,25 +176,64 @@ function operate(stringVal) {
 
 	let displayContainer = document.querySelector('#display');
 	let answerDisplay = document.querySelector('#currentDisplay');
-	
+
 	answerDisplay.textContent = returnDisplay;
 	displayContainer.appendChild(answerDisplay);
+
+	currentDisplay = "Answered";
 
 
 }
 
 function multiply(num1, num2) {
-	return num1 * num2;
+	let one = parseInt(num1 * 100000);
+	let two = parseInt(num2 * 100000);
+	let bigAns = one * two;
+	let ans = bigAns/ 10000000000;
+	return ans;
 }
 
 function divide(num1, num2) {
-	return num1 / num2;
+	if (num2 == 0) {
+		return 'Error';                  //Catching divide by zero 
+	}
+	else {
+		let one = parseInt(num1 * 100000);
+		let two = parseInt(num2 * 100000);
+		let bigAns = one / two;
+		let medAns = parseInt(bigAns * 100000)
+		let ans = medAns / 100000;
+	return ans;
+	}
 }
 
 function add(num1, num2) {
-	return num1 + num2;
+	let one = parseInt(num1 * 100000);
+	let two = parseInt(num2 * 100000);
+	let bigAns = one + two;
+	let ans = bigAns / 100000;
+	return ans;
 }
 
 function subtract(num1, num2) {
-	return num1 - num2;
+	let one = parseInt(num1 * 100000);
+	let two = parseInt(num2 * 100000);
+	let bigAns = one - two;
+	let ans = bigAns / 100000;
+	return ans;
+}
+
+function decimalCheck(input) {
+	let returnVal = 'yes';
+	for (let r = equationArray.length; r > -1; r--) {
+		if (equationArray[r] == input) {
+			returnVal = "no";
+			break;
+		}
+		if (equationArray[r] == ' + ' || equationArray[r] == ' - ' || equationArray[r] == ' * ' || equationArray[r] == ' / ') {
+			returnVal == 'yes';
+			break;
+		}
+	}
+	return returnVal;
 }
